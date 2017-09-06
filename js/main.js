8b6cb73dc73a70371b30e116e09d1cbe80581d79
Michael Sharman <sharmo@gmail.com>
2017-09-06 14:39:42 +1000

    Removed local materialize files, added legend for component status

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.button-collapse').sideNav();
+
+    // Set the indeterminate value of checkboxes
+    // based on a classname
+    var indeterminateCheckboxes = document.getElementsByClassName('indeterminate');
+    for (var i = 0; i < indeterminateCheckboxes.length; i++) {
+        indeterminateCheckboxes[i].indeterminate = true;
+    }
 });
